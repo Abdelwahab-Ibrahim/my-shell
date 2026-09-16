8be485be2d47e932aaddbf86ec62990625133030
Abd-elwahab Ibrahim <abdelwahab.ibrahim.wahba@gmail.com>
2026-09-16 14:48:18 +0300

    codecrafters submit [skip ci]

---
+++ src/lexer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+#include <string>
+using namespace std;
+enum class State
+{
+    Plain,
+    InSingleQuote,
+    InDoubleQuote,
+    EscapedOut,
+    EscapedInDoubleQuote,
+    Redirect,
+    Collect
+};
+
+enum class TokenType
+{
+    WORD,
+    STD_OUT,
+    STD_OUT_APPEND,
+    STD_ERR,
+    STD_ERR_APPEND,
+    EOL
+};
+
+struct Token
+{
+    TokenType type;
+    string value;
+};
+
+std::vector<Token> tokenize(const std::string &cmd);
