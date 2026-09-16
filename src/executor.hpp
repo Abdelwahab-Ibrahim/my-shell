9a9eef9046899327d055b3de2e68544f51e987d0
Abd-elwahab Ibrahim <abdelwahab.ibrahim.wahba@gmail.com>
2026-09-16 13:56:32 +0300

    codecrafters submit [skip ci]

--- src/executor.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef EXECUTOR_HPP
-#define EXECUTOR_HPP
-
-#include <string>
-#include <vector>
-
-void run_external(const std::string& path, const std::vector<std::string>& args);
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,18 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <sstream>
-
-#include <unistd.h>
-#include <cstdlib>
-#include <csignal>
-#include <filesystem>
-#include <sys/wait.h>
-#include <sys/types.h>
-
-
-#include "executor.hpp"
-#include "builtins.hpp"
-#include "utils.hpp"
-#include "style.cpp"
 using namespace std;
+int main() {
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
 
-string getCurrentDirectory()
-{
-    // replace home with ~
-    string home = getenv("HOME");
-    if (home.empty()){
-        return filesystem::current_path().string();
-    }
-    // Replace home with ~
-    string current_dir = filesystem::current_path().string();
-    if (current_dir.substr(0, home.length()) == home) {
-        current_dir = "~" + current_dir.substr(home.length());
-    }
-    return current_dir;
-}
-
-int main()
-{
-    cout << unitbuf;
-    cerr << unitbuf;
-
-    bool running = true;
-
-    while (running)
-    {
-        cout << PRIMARY << getCurrentDirectory() << WARNING << " $ " << MUTED;
-
-        string command;
-        getline(cin, command);
-
-        auto [cmd, args] = parseCommand(command);
-
-        if (cmd.empty())
-            continue;
-
-        if (isBuiltin(cmd))
-        {
-            executeBuiltin(cmd, args, running);
-        }
-        else
-        {
-            string path = checkCMD(cmd);
-
-            if (!path.empty())
-            {
-                vector<string> argv = split(command);
-                run_external(path, argv);
-            }
-            else
-            {
-                cout << ERROR << cmd << RESET
-                     << ": command not found" << endl;
-            }
-        }
-    }
-
-    return 0;
+  // TODO: Uncomment the code below to pass the first stage
+  // the loop
+  while (true)
+  { 
+    std::cout << "$ ";
+    string command;
+    std::getline(std::cin, command);
+    cout << command << ": command not found"<< endl;
+  }
 }
